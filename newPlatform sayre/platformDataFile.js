e7306bec83571a941bba7b65026b62bd30189263
on606 <steepanddeep@gmail.com>
2018-03-14 10:50:30 -0600

    Upload Platform data

---
+++ newPlatform sayre/platformDataFile.js
@@ -0,0 +1,35 @@
+(function () {
+    platformData = [
+        {
+            "name": "Molecule Man",
+            "age": 29,
+            "secretIdentity": "Dan Jukes",
+            "powers": [
+                "Radiation resistance",
+                "Turning tiny",
+                "Radiation blast"
+            ]
+        },
+        {
+            "name": "Madame Uppercut",
+            "age": 39,
+            "secretIdentity": "Jane Wilson",
+            "powers": [
+                "Million tonne punch",
+                "Planet cracker",
+                "Freeze ray",
+                "Damage resistance",
+                "Superhuman reflexes"
+            ]
+        },
+        {
+            "name": "Sayre Thomas",
+            "age": 39,
+            "secretIdentity": "Jane Wilson",
+            "powers": [
+                "Million tonne punch",
+                "Planet cracker"
+            ]
+        }
+    ];
+})();
